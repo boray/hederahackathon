82aa32ad223c476a1f68f685133c54834cf4820e
“boray” <bsaygilier33@gmail.com>
2022-05-05 01:53:21 +0300

    fidelio

--- contracts/main.sol
+++ contracts/main.sol
@@ -3,8 +3,20 @@ pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+
+//Donator ===> Donate Pool ===> Validators checks student docs & sets withdraw allowance  ===> Student withdraws eth
+
+//Epoch : Weekly, Biweekly or Monthly cycles for validating student docs and setting allowances
+
+
+
+
 contract Main is Ownable {
+		
 	
+	event WithdrawAsStudent(address withdrawer, uint256 amount);
+
 	struct Student {
 	address receiverAddress;
 	uint256 receivedEpoch;
@@ -20,17 +32,37 @@ contract Main is Ownable {
 	mapping(uint256 => Student) private students;
 	mapping(uint256 => Donator) private donators;
 	mapping(address => bool) private validators;
-
-
+	mapping(address => uint256) private withdrawAllowance;
+	
+	modifier onlyValidator() {
+		require(validators[msg.sender],'YOU ARE NOT VALIDATOR!');
+	
+	}
 	function addValidator(address newValidator) public onlyOwner {
-	validators[newValidator] = true;
+		validators[newValidator] = true;
 	}
 	
         function removeValidator(address newValidator) public onlyOwner {
-        validators[newValidator] = true;
+        	validators[newValidator] = true;
         }
 	
+	function validateStudentEpochAllowance() public onlyValidator {
+	
+	}
 
+	function withdrawAsStudent() public {
+	uint256 amount_ = withdrawAllowance[msg.sender];
+	withdrawAllowance[msg.sender] = 0;	
+	payable(msg.sender).transfer(amount_);
+
+        emit WithdrawAsStudent(msg.sender,amount_);
+	}
+
+	function donate() public payable {
+        
+	
+	
+	}
 
 }
 
